95c59f7efb9d94d007cfc431ab042471009bc722
Edmond <97a.edmond@gmail.com>
2022-10-22 18:04:32 +0200

    Add files via upload

---
+++ TodoItem.java
@@ -0,0 +1,29 @@
+package project.todolist;
+
+import java.time.LocalDate;
+
+public class TodoItem {
+
+    private final String shortDescription;
+    private final String details;
+    private final LocalDate deadline;
+
+    public TodoItem(String shortDescription, String details, LocalDate deadline) {
+        this.shortDescription = shortDescription;
+        this.details = details;
+        this.deadline = deadline;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+}
